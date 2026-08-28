401696a07608012c22aab7896c7a6701ee82a2e4
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-28 21:59:50 +0530

    chore: add provider package boundary

---
+++ src/refute/providers/__init__.py
@@ -0,0 +1,9 @@
+from ..llm import LLM, LLMError, OllamaLLM, OpenAICompatibleLLM, provider_from_env
+
+__all__ = [
+    "LLM",
+    "LLMError",
+    "OllamaLLM",
+    "OpenAICompatibleLLM",
+    "provider_from_env",
+]
