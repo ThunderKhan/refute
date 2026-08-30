57789cae0d19f31eb33dfb6c34eb2a7a96d4182b
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-30 17:33:03 +0530

    feat: route verify page to live dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import LiveVerifyPage from "./LiveVerifyPage";
 import "./styles.css";
 import "./polish.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root")!;
+const isLiveVerifyRoute = window.location.pathname === "/verify";
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {isLiveVerifyRoute ? <LiveVerifyPage /> : <App />}
     </BrowserRouter>
   </React.StrictMode>,
 );
