255e0ef9ce6e2fc1e6b25653fbf9a5f8463eb16c
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-30 19:22:05 +0530

    Support GitHub task-list markdown

--- frontend/src/MarkdownBlock.tsx
+++ frontend/src/MarkdownBlock.tsx
@@ -1,5 +1,10 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function MarkdownBlock({ children }: { children: string }) {
-  return <div className="markdown-block"><ReactMarkdown>{children}</ReactMarkdown></div>;
+  return (
+    <div className="markdown-block">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
+    </div>
+  );
 }
